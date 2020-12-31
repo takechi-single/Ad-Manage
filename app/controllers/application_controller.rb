2ff66f3b769507b0a37b7f163f79b3b2bd09c5de
takechi-single <t.takaaki0906@gmail.com>
2020-12-31 18:13:47 +0900

    認証機能の変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -11,7 +10,5 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
+  
 end
