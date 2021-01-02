47fe045ff6ecd4f535ff880141f10a1946d5fc2f
takechi-single <t.takaaki0906@gmail.com>
2021-01-02 09:54:16 +0900

    mediaバリデーションの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new
+    if @user.valid?
+      @user.save
+      redirect_to action: :index
+    end
+  end
+
   def edit; end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'manages#index'
-  resources :users, only: %i[edit update show]
+  resources :users, only: %i[edit update show, new, create, index]
   resources :items, only: %i[index new create show] 
   resources :manages, only: %i[index  show new] 
 end
