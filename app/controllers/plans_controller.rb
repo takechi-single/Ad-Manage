146d01e326ab8fccc235f81a5e3d4e35ec899408
takechi-single <t.takaaki0906@gmail.com>
2021-01-02 17:31:11 +0900

    広告登録画面への遷移実装

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,5 +1,13 @@
 class PlansController < ApplicationController
-  
+  before_action :set_item, only: [:index, :create, :new]
+
+  def index
+    
+  end
+
+  def new
+    
+  end
 
   def create
     plan = Plan.create(plan_params)
@@ -9,6 +17,10 @@ class PlansController < ApplicationController
   private
   def plan_params
     params.require(:plan).permit(:when, :when_by, :where, :who, :with_whom, :whom, :what, :why, :how, :how_many, :how_much).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'manages#index'
   resources :users, only: %i[edit update show, new, create, index]
   resources :items, only: %i[index new create show] do
-    resources :plans, only: %i[create]
+    resources :plans, only: %i[index new create]
   end
 
   resources :manages, only: %i[index  show new] 
