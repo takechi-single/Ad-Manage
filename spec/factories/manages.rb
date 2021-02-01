9bb8d5977ac3c4c3d4c810ba4a3f937a396b676c
takechi-single <t.takaaki0906@gmail.com>
2021-02-01 21:42:10 +0900

    各モデルのテストコード記載

--- spec/factories/manages.rb
+++ spec/factories/manages.rb
@@ -1,4 +1,6 @@
 FactoryBot.define do
   factory :manage do
+    sale_date { '2021-02-01' }
+    profit { '50000' }
   end
 end
--- spec/factories/plans.rb
+++ spec/factories/plans.rb
@@ -1,4 +1,8 @@
 FactoryBot.define do
   factory :plan do
+   when { '2021-01-01' }
+   target_id { '男女35歳～49歳' } 
+   media_id { '折込' } 
+   how_much { '460000' } 
   end
 end
